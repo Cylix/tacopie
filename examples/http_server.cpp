24737bbe2d99a7ebd3780c7042bfb0ae83d5613e
Simon Ninon <simon.ninon@gmail.com>
2016-10-22 00:35:13 +0200

    logger example & wiki asset

--- examples/http_server.cpp
+++ examples/http_server.cpp
@@ -5,5 +5,7 @@ main(void) {
   //! enable logging
   cpp_http_server::active_logger = std::unique_ptr<cpp_http_server::logger>(new cpp_http_server::logger(cpp_http_server::logger::log_level::debug));
 
+  __CPP_HTTP_SERVER_LOG(info, "Hello World!");
+
   return 0;
 }
---
+++ examples/logger.cpp
@@ -0,0 +1,70 @@
+#include <cpp_http_server/cpp_http_server>
+
+#include <iostream>
+
+class my_logger : public cpp_http_server::logger_iface {
+public:
+  //! ctor & dtor
+  my_logger(void)  = default;
+  ~my_logger(void) = default;
+
+  //! copy ctor & assignment operator
+  my_logger(const my_logger&) = default;
+  my_logger& operator=(const my_logger&) = default;
+
+public:
+  void
+  debug(const std::string& msg, const std::string& file, std::size_t line) {
+    std::cout << "debug: " << msg << " @ " << file << ":" << line << std::endl;
+  }
+
+  void
+  info(const std::string& msg, const std::string& file, std::size_t line) {
+    std::cout << "info: " << msg << " @ " << file << ":" << line << std::endl;
+  }
+
+  void
+  warn(const std::string& msg, const std::string& file, std::size_t line) {
+    std::cout << "warn: " << msg << " @ " << file << ":" << line << std::endl;
+  }
+
+  void
+  error(const std::string& msg, const std::string& file, std::size_t line) {
+    std::cerr << "error: " << msg << " @ " << file << ":" << line << std::endl;
+  }
+};
+
+
+int
+main(void) {
+  //! By default, no logging
+  //! Force logger call, just for the example (you will never have to do that by yourself)
+  std::cout << "By default: no logging" << std::endl;
+  __CPP_HTTP_SERVER_LOG(debug, "This is a debug message");
+  __CPP_HTTP_SERVER_LOG(info, "This is an info message");
+  __CPP_HTTP_SERVER_LOG(warn, "This is a warn message");
+  __CPP_HTTP_SERVER_LOG(error, "This is an error message");
+  std::cout << std::endl;
+
+  //! Use the default logger, provided with the library
+  cpp_http_server::active_logger = std::unique_ptr<cpp_http_server::logger>(new cpp_http_server::logger(cpp_http_server::logger::log_level::debug));
+  //! Force logger call, just for the example (you will never have to do that by yourself)
+  std::cout << "With the library provided logger" << std::endl;
+  __CPP_HTTP_SERVER_LOG(debug, "This is a debug message");
+  __CPP_HTTP_SERVER_LOG(info, "This is an info message");
+  __CPP_HTTP_SERVER_LOG(warn, "This is a warn message");
+  __CPP_HTTP_SERVER_LOG(error, "This is an error message");
+  std::cout << std::endl;
+
+  //! Use your custom logger
+  cpp_http_server::active_logger = std::unique_ptr<my_logger>(new my_logger);
+  //! Force logger call, just for the example (you will never have to do that by yourself)
+  std::cout << "With an example of custom logger" << std::endl;
+  __CPP_HTTP_SERVER_LOG(debug, "This is a debug message");
+  __CPP_HTTP_SERVER_LOG(info, "This is an info message");
+  __CPP_HTTP_SERVER_LOG(warn, "This is a warn message");
+  __CPP_HTTP_SERVER_LOG(error, "This is an error message");
+  std::cout << std::endl;
+
+  return 0;
+}
