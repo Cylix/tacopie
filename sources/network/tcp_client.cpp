c49c2ba49428b76630ef70fd3f830bd618a05cc6
Simon Ninon <simon.ninon@gmail.com>
2017-04-29 22:46:56 -0700

    [2.4.1] fix behavior when trying to reconnect from disconnection_handler callback.

--- sources/network/tcp_client.cpp
+++ sources/network/tcp_client.cpp
@@ -61,8 +61,14 @@ void
 tcp_client::connect(const std::string& host, std::uint32_t port) {
   if (is_connected()) { __TACOPIE_THROW(warn, "tcp_client is already connected"); }
 
-  m_socket.connect(host, port);
-  m_io_service->track(m_socket);
+  try {
+    m_socket.connect(host, port);
+    m_io_service->track(m_socket);
+  }
+  catch (const tacopie_error& e) {
+    m_socket.close();
+    throw e;
+  }
 
   m_is_connected = true;
 
