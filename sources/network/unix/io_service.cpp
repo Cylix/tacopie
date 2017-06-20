2f2b159340cad1a90304fe73838259f8d059a0d3
Simon Ninon <simon.ninon@whova.com>
2017-06-19 23:03:51 -0700

    [2.4.3] try to address high CPU usage issues by ádding ability to configure select timeout at compile time. (#14)

--- sources/network/unix/io_service.cpp
+++ sources/network/unix/io_service.cpp
@@ -87,8 +87,16 @@ io_service::poll(void) {
   while (!m_should_stop) {
     int ndfs = init_poll_fds_info();
 
+    //! setup timeout
+    struct timeval* timeout_ptr = NULL;
+#ifdef __TACOPIE_TIMEOUT
+    struct timeval timeout;
+    timeout.tv_usec = __TACOPIE_TIMEOUT;
+    timeout_ptr     = &timeout;
+#endif /* __TACOPIE_TIMEOUT */
+
     __TACOPIE_LOG(debug, "polling fds");
-    if (::select(ndfs, &m_rd_set, &m_wr_set, NULL, NULL) > 0) { process_events(); }
+    if (::select(ndfs, &m_rd_set, &m_wr_set, NULL, timeout_ptr) > 0) { process_events(); }
     else {
       __TACOPIE_LOG(debug, "poll woke up, but nothing to process");
     }
